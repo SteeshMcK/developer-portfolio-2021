001e26f99de6005494adc84620c832b3fcfadaf4
SteeshMcK <stesha.norstrom@pcc.edu>
2021-02-08 21:08:33 -0800

    add styling to header mobile view

--- components/Header.js
+++ components/Header.js
@@ -1,17 +1,11 @@
-// styled from the global.css file
-
 const Header = () => {
     return ( 
         <header>
             <div>
                 <p>Stesha McKindle + Web Creations</p>
             </div>
-            <div className='contact'>
-                <p>email: steshamckindle@gmail.com</p>
-                <p>phone: 503-840-9044</p>
-            </div>
         </header>
      );
-}
+} 
  
 export default Header;
