49b36f3f2801fadac83616b2bc59a651bebe1fc9
SteeshMcK <stesha.norstrom@pcc.edu>
2021-02-09 21:07:30 -0800

    add link to projects page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -18,7 +19,9 @@ export default function Home() {
           <p>phone: 503-840-9044</p>
         </div>
         <div className='projects'>
-          <p>link to projects</p>
+          <Link href='./projects'>
+            <a>Projects</a>
+          </Link>
         </div>
       </main>
     </div>
